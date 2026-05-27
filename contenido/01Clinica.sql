a1c89818ead25df7c00edf5f519401dbd07e2ca0
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-05-27 13:49:29 -0600

    Auto 2026-05-27 13:49

---
+++ contenido/01Clinica.sql
@@ -0,0 +1,38 @@
+USE CLINICA
+
+01 Mostrar los datos del paciente 100 y 200
+
+02 Proporcione el nombre de los pacientes que han estado 
+    en la habitacion = 201, pues se ha dectectado un virus	 
+
+03 Mostrar el Nombre Completo del paciente,
+   es decir, en una sola columna, y la edad 
+   Ordenados por Edad,
+
+04 Pacientes presentando su edad,
+   Si es mayor a 60 que diga "Sesenta y mas"
+   De lo contrario, "Normal"	
+   
+05 Cuantos medicos se tienen por Especialidad,
+   Mostrando el nombre de la especialidad
+   
+06 Cuantos tipos de cama existen.
+
+07 Ingresos economicos por especialidad
+
+08 Numero de citas por especialidad
+
+09 Doctor que mas citas atiende.
+
+10 Se desea separar en Pacientes los Apellidos en:
+   Paterno y Materno.
+   Realizar una consulta que muestre la separacion
+   
+11 Doctores que no tienen consultas.
+
+   
+   
+
+
+   
+   
---
+++ contenido/01Mexico.sql
@@ -0,0 +1,40 @@
+Use MEXICO;
+
+01 Obtener el numero total de habitantes por partido político
+
+02 Gobernadores que entraron por año o 
+   en que año entraron mas gobernadores
+
+03 cual es la ciudad que mas municipios tiene
+
+04 cuales  son las ladas del estado de aguascalientes
+
+05 Cuantas son las ladas del estado de aguascalientes
+
+06 Que gobernadores entraron en fin de semana (sabado o domingo) o cuantos
+
+07 Mostrar la siguiente informacion del estado de AGS
+   con sus 11 municipios ordenado por municipio
+   ABR, Nombre_Gobernador,  Nombre_Municipio
+
+08 Muestra los empleados que tengan mas de un nombre
+
+09 Mostrar las ciudades cuyo nombre estan entre la D y J
+
+10 En la tabla de Empleados ingresa tus datos por codigo:
+
+11 Al empleado 100 actualizar por Codigo:
+   el sexo a Hombre,
+   En la ciudad de aguascalientes
+   En el municipio de Asientos
+
+12 Sexo DDL y DML
+   Se sabe que los estados de:
+   Chiapas y Sonora es gobernado por Mujeres
+
+   Por medio de Codigo realizar lo siguiente:
+   En la tabla de Ciudades:
+   * Crear un atributo sexo de tipo character
+     que contenga ya sea 'H' que es Hombre
+                         'M' que es Mujer
+   * Colocar pues el sexo respectivo a todas las ciudades
