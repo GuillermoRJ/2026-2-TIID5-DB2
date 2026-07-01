851323427eaa9400d33ab5d6f71f8ff21809bcb8
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-07-01 16:09:50 -0600

    Auto 2026-07-01 16:09

--- src/funciones.sql
+++
@@ -1,8 +0,0 @@
--- Active: 1778258982896@@127.0.0.1@3306@HR
--- Cuantos empleados ganan mas que cada empleado
-SELECT EMPLOYEE_ID, COUNT(*)
-FROM employees e
-WHERE salary > (select salary
-                from employees x
-                where e.EMPLOYEE_ID = x.EMPLOYEE_ID)
-group by employee_id; 
